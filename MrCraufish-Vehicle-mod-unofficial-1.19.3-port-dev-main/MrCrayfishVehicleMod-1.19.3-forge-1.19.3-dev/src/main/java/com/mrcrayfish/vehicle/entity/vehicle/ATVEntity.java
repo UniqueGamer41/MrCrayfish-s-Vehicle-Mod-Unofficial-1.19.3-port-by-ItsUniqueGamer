d55e6e8cb961124806af96993c07e45523a4dcb5
UniqueGamer41 <117046818+UniqueGamer41@users.noreply.github.com>
2024-08-19 15:14:06 +0530

    Added ATVEntity
    Added entity for ATV

---
+++ MrCraufish-Vehicle-mod-unofficial-1.19.3-port-dev-main/MrCrayfishVehicleMod-1.19.3-forge-1.19.3-dev/src/main/java/com/mrcrayfish/vehicle/entity/vehicle/ATVEntity.java
@@ -0,0 +1,10 @@
+package com.mrcrayfish.vehicle.entity.vehicle;
+
+import com.mrcrayfish.vehicle.entity.LandVehicleEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class ATVEntity extends LandVehicleEntity
+{
+    public ATVEntity(EntityType<? extends ATVEntity> type, Level worldIn) {super(type, worldIn);}
+}
